df4d3634a03e364cd9df73e7ef5c8745587b7958
Jim Schlader <jamesschlader@yahoo.com>
2019-09-16 10:44:03 -0600

    added more user validation

--- Models/User.js
+++ Models/User.js
@@ -2,20 +2,53 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  first: { type: String, required: true },
-  last: String,
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+  first: {
+    type: String,
+    required: true,
+    trim: true,
+    match: [/\D/g, "please only use letters in the first name"],
+    minlength: [1, "please provide a first name"],
+    maxlength: [30, "first name cannot be more than 30 characters"]
+  },
+  last: {
+    type: String,
+    required: true,
+    match: [/\D/g, "please only use letters in the last name"],
+    trim: true,
+    minlength: [1, "please provide a last name"],
+    maxlength: [40, "last name cannot be more than 40 characters"]
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: [5, "username must be at least 5 characters"],
+    maxlength: [30, "username cannot be more than 30 characters"]
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    minlength: [8, "password must be at least 8 characters"],
+    maxlength: [30, "password cannot be more than 30 characters"]
+  },
   email: {
     type: String,
-    validate: {
-      validator: function(target) {
-        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(target);
-      },
-      message: props => `${props} is not a valid email.`
-    }
+    required: true,
+    lowercase: true,
+    trim: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "given input is not a valid email address"
+    ]
   },
-  motto: { type: String },
+  motto: {
+    type: String,
+    trim: true,
+    maxlength: [220, "the motto cannot exceed 220 characters"]
+  },
   stable: [Schema.Types.ObjectId],
   image: { type: String }
 });
